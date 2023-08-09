7e53cf22cfde009153a8af3f7eb42e304a321a60
Dj Amie X <32149397+djamiex@users.noreply.github.com>
2023-08-09 20:14:31 +0800

    Create my.lua

---
+++ esx_skin/locales/my.lua
@@ -0,0 +1,6 @@
+Locales["my"] = {
+  ["skin_menu"] = "Skin Menu",
+  ["use_rotate_view"] = "use ~INPUT_FRONTEND_LS~ and ~INPUT_CHARACTER_WHEEL~ to rotate the view.",
+  ["skin"] = "change skin",
+  ["saveskin"] = "save skin to a file",
+}
